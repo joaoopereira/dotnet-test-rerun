771858450c6152e15087d573da30d40874a3ceee
João Pereira <joaopereira@criticalmanufacturing.com>
2023-02-14 14:37:40 +0000

    fix(entrypoint): replace try catch with UseExceptionHandler

--- src/Program.cs
+++ src/Program.cs
@@ -2,6 +2,7 @@
 using System.CommandLine.Builder;
 using System.CommandLine.Parsing;
 using System.IO.Abstractions;
+using System.Reflection;
 using dotnet.test.rerun;
 using dotnet.test.rerun.Logging;
 using Microsoft.Extensions.DependencyInjection;
@@ -18,21 +19,21 @@ var serviceProvider = new ServiceCollection()
 var Log = serviceProvider.GetRequiredService<ILogger>();
 var cmd = serviceProvider.GetService<RerunCommand>();
 
-try
-{
-    return await new CommandLineBuilder(cmd)
-        .UseDefaults()
-        .Build()
-        .InvokeAsync(args);
-}
-catch (RerunException e)
-{
-    Log.Error(e.Message);
-    Log.Debug(e.StackTrace ?? string.Empty);
-}
-catch (Exception e)
-{
-    Log.Exception(e);
-}
-
-return -1;
+return await new CommandLineBuilder(cmd)
+    .UseDefaults()
+    .UseExceptionHandler((exception, context) =>
+    {
+        if (exception is RerunException
+            || (exception is TargetInvocationException
+            && exception.InnerException is RerunException))
+        {
+            Log.Error(exception.Message);
+            Log.Debug(exception.StackTrace ?? string.Empty);
+        }
+        else
+        {
+            Log.Exception(exception);
+        }
+    })
+    .Build()
+    .InvokeAsync(args);
