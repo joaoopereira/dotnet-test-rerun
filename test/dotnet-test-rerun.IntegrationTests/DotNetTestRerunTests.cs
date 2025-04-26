e1fd12f15696565360a8ab95a668d86a286028c3
Vadim_Nesterov <alan@xaker.ru>
2025-04-27 01:47:58 +0300

    added test that fails if target unit test has Theory.DisplayName specified

--- test/dotnet-test-rerun.IntegrationTests/DotNetTestRerunTests.cs
+++ test/dotnet-test-rerun.IntegrationTests/DotNetTestRerunTests.cs
@@ -188,6 +188,22 @@ public class DotNetTestRerunTests
     }
 
     [Fact]
+    public async Task DotnetTestRerun_FailingXUnit_FailsInAllRetriesWithDisplayName()
+    {
+        // Arrange
+        Environment.ExitCode = 0;
+
+        // Act
+        var output = await RunDotNetTestRerunAndCollectOutputMessage("XUnitExampleFailInAllRetriesWithDisplayName");
+
+        // Assert
+        output.Should().Contain("Failed!", Exactly.Times(4));
+        output.Should().Contain("Failed:     2, Passed:     0", Exactly.Times(4));
+        Environment.ExitCode.Should().Be(1);
+        IsThereACoverageFile().Should().BeFalse();
+    }
+
+    [Fact]
     public async Task DotnetTestRerun_FailingXUnit_PassOnSecond()
     {
         // Arrange
