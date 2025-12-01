b2f02d05b859f6cd45b7e5c94bcbd3e50c5ef961
João Octávio Pereira <mail@joaoopereira.com>
2025-12-01 16:45:21 +0000

    test: add sequential collection for LoggerTests to disable parallelization

---
+++ test/dotnet-test-rerun.UnitTests/Logging/SequentialCollection.cs
@@ -0,0 +1,9 @@
+using Xunit;
+
+namespace dotnet_test_rerun.UnitTest.Logging;
+
+[CollectionDefinition("Sequential", DisableParallelization = true)]
+public class SequentialCollection
+{
+    // This class is intentionally empty. It exists only to define the collection.
+}
